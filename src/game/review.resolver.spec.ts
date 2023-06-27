3fb5b18ec2d8e0da4b27fc531bcc2e582c5c739e
Mehdi <me.moozeh@gmail.com>
2023-06-27 14:49:12 +0300

    test: new review

--- src/game/review.resolver.spec.ts
+++ src/game/review.resolver.spec.ts
@@ -1,18 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewResolver } from './review.resolver';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../app.module';
+import * as request from 'supertest';
 
 describe('ReviewResolver', () => {
-  let resolver: ReviewResolver;
+  let app: INestApplication;
+  let accessToken: string;
+  let gameId: string;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewResolver],
+  // TODO: drop collections before start
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
-
-    resolver = module.get<ReviewResolver>(ReviewResolver);
+    app = moduleFixture.createNestApplication();
+    await app.init();
+    const fakeUser = {
+      email: 'mee.moozeh@gmail.com', // TODO: generate random email
+      password: 'Mehdi@123456',
+    };
+    const newUser = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+        mutation {
+          register(registerInput: {
+            email: "${fakeUser.email}",
+            password: "${fakeUser.password}",
+          }) {
+            _id
+            token
+          }
+        }`,
+      })
+      .expect(200);
+    accessToken = newUser.body.data.register.token;
+    const newGame = await request(app.getHttpServer())
+      .post('/graphql')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        query: `
+        mutation {
+          newGame(newGameInput: {
+            title: "game for test 1"
+            averageRating: 3.5,
+            description: "awesome game",
+            releaseDate: "2023-12-15",
+          }) {
+            _id
+          }
+        }`,
+      })
+      .expect(200);
+    gameId = newGame.body.data.newGame._id;
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  test('Post a new review', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        query: `
+        mutation {
+          newReview(NewReviewInput: {
+            gameId: "${gameId}",
+            rating: 4.5,
+            comment: "awesome game",
+          }) {
+            _id
+          }
+        }`,
+      })
+      .expect(200);
+    console.log(response.body.data.newReview._id);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
